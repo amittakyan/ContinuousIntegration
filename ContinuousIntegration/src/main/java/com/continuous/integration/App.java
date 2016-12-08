765efbc45e7465ae90d1e0af82589d5fc417656f
Amit Saini <asaini@miraclesoft.com>
2016-12-08 11:51:24 -0500

    Change value G

--- ContinuousIntegration/src/main/java/com/continuous/integration/App.java
+++ ContinuousIntegration/src/main/java/com/continuous/integration/App.java
@@ -2,8 +2,8 @@ package com.continuous.integration;
 
 public class App {
 
-	public String conCat(String a, String bb){
-		return a+bb;
+	public String conCat(String a, String g){
+		return a+g;
 	}
 	
 	public int add (int a, int b){
